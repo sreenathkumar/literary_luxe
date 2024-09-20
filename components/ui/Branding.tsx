130ced031487a358dcf8689627d6f1e282ed04c5
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-09-20 16:04:25 +0600

    fix: changed the text color class

--- components/ui/Branding.tsx
+++ components/ui/Branding.tsx
@@ -4,7 +4,7 @@ function Branding() {
     return (
         <div className={cn("gap-4 flex-col justify-start items-start inline-flex md:max-w-[324px]")}>
             <h3 className={"self-stretch font-primary text-h3 text-neutral font-semibold leading-loose"}>LiteraryLuxe</h3>
-            <p className={cn('font-primary text-body text-neutral-800')}>Where the love of reading meets convenience. Explore our curated best sellers, trending titles, and timeless classics. Join our community and elevate your literary journey. </p>
+            <p className={cn('font-primary text-body text-neutral-700')}>Where the love of reading meets convenience. Explore our curated best sellers, trending titles, and timeless classics. Join our community and elevate your literary journey. </p>
         </div>
     )
 }
