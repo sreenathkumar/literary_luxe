847ab1bfb66d145d6f814ecdd3a325056522e753
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-09-22 15:20:37 +0600

    feat: added z index to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Menu from './ui/Menu'
 
 function Header() {
     return (
-        <header className='p-4 sticky top-0 bg-primary md:px-0'>
+        <header className='p-4 sticky top-0 bg-primary z-40 md:px-0'>
             <div className="md:container flex items-center justify-between">
                 <Logo text='LiteraryLuxe' />
                 <div className="flex gap-10">
