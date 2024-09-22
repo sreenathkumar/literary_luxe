e2d7577cfd542840388023e98073d1e33579a9ff
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-09-22 15:17:42 +0600

    feat: added the url to the hero button and adjusted some css

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,18 +1,22 @@
 import Container from "@/components/ui/Container";
-import H1 from "@/components/ui/H1";
+import H1 from "@/components/globalTags/H1";
 import { Button } from "@shadcn/button";
+import Link from "next/link";
+import Section from "../globalTags/Section";
 
 function HeroSection() {
     return (
-        <section className="justify-between items-center inline-flex">
-            <Container classname="py-[72px] items-start">
+        <Section>
+            <Container>
                 <div className="flex flex-col md:max-w-[600px]">
                     <H1>Get the best books to read and enjoy.</H1>
-                    <p className="text-neutral-700 text-lg tracking-wide leading-relaxed mt-6">Discover the books that captivate readers worldwide. Explore what people are reading and loving across the globe, and delve into their reviews and insights. Share your thoughts and opinions on these books, adding your unique perspective to the literary conversation.</p>
+                    <p className="text-neutral-200 text-lg tracking-wide leading-relaxed mt-6">Discover the books that captivate readers worldwide. Explore what people are reading and loving across the globe, and delve into their reviews and insights. Share your thoughts and opinions on these books, adding your unique perspective to the literary conversation.</p>
                 </div>
-                <Button className="mt-10 py-3 px-5 text-primary text-base font-medium">Explore Now!</Button>
+                <Button className="mt-10 py-3 px-5 text-primary text-base font-medium">
+                    <Link href='/books'>Explore Now!</Link>
+                </Button>
             </Container>
-        </section>
+        </Section>
     )
 }
 
