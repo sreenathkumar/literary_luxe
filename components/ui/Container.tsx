d78320d43ea1900db024e3059b0a36a23a9e62ab
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-09-22 15:18:32 +0600

    fix: added flex allignment

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 
 function Container({ children, classname }: { children: React.ReactNode, classname?: string }) {
     return (
-        <div className={cn(`flex flex-col px-4 md:container ${classname ? classname : ''}`)}>
+        <div className={cn(`flex flex-col items-start md:container ${classname ? classname : ''}`)}>
             {children}
         </div>
     )
