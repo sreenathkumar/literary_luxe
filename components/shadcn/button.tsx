5628ae3b90176936736be8a527fecb5b89f78a1b
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-09-22 15:19:19 +0600

    fix: chaged te defult button colors(border, bg text)

--- components/shadcn/button.tsx
+++ components/shadcn/button.tsx
@@ -5,22 +5,22 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center font-primary whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center font-primary whitespace-nowrap rounded-md text-base tracking-widest font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:text-neutral disabled:pointer-events-none disabled:opacity-50 hover:border-accent hover:bg-transparent hover:text-accent",
   {
     variants: {
       variant: {
-        default: "bg-accent text-neutral border border-accent hover:border-neutral hover:bg-transparent",
+        default: "bg-accent text-primary border border-accent shadow-sm",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input text-neutral hover:bg-accent hover:text-neutral hover:border-accent",
+          "border border-accent text-accent hover:bg-accent hover:text-primary hover:border-accent",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "px-4 py-2",
+        default: "px-5 py-3",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
