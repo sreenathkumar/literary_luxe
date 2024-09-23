bc91e4d30e07abdd54d5122ccafb45b561665979
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-09-23 14:39:13 +0600

    fix: replaced imge.domains with remotepatterns as it was depreciated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["via.placeholder.com", "storage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
+    ],
   },
 };
 
